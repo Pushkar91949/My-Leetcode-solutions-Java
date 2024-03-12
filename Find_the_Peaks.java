f6c0e0a4ab8360c10642c72446d0105c9cd740a8
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-03-12 23:14:03 +0530

    Create Find_the_Peaks.java

---
+++ Find_the_Peaks.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> peaks = new ArrayList<>();
+        for (int i = 1; i < mountain.length - 1; i++) {
+            if (mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1]) {
+                peaks.add(i);
+            }
+        }
+        return peaks;
+    }
+}
+// Question link: https://leetcode.com/problems/find-the-peaks/
