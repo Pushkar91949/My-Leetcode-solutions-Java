5f48088b2561bbe121556e41abb47a7d4dd2e7a5
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-08-01 20:17:20 +0530

    Create Number_of_Senior_Citizens.java

---
+++ Number_of_Senior_Citizens.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int ans = 0;
+        for (var x:details) {
+            int a = Integer.parseInt(x.substring(11,13));
+            if (a > 60) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
