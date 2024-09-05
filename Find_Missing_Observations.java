2d0b6218e80da6cc45476ff2e6b952348c1859f2
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-09-05 09:22:01 +0530

    Create Find_Missing_Observations.java

---
+++ Find_Missing_Observations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int msum = 0;
+        for (int roll : rolls) {
+            msum += roll;
+        }
+        int m = rolls.length;
+        int nsum = (mean * (n + m)) - msum;
+
+        if (nsum > n * 6 || nsum < n) {
+            return new int[0];
+        }
+
+        int nmean = nsum / n;
+        int nmod = nsum % n;
+        int[] ans = new int[n];
+        Arrays.fill(ans, nmean);
+
+        for (int i = 0; i < nmod; i++) {
+            ans[i] += 1;
+        }
+
+        return ans;
+    }
+}
