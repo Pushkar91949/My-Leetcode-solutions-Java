6b01f2df700db5b87ae233aeae0f858c903298b5
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-12-14 22:56:15 +0530

    Create Continuous_Subarrays.java

---
+++ Continuous_Subarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int l = 0, r = 0;
+        long ans = 0;
+
+        while (r < nums.length) {
+            freq.put(nums[r], freq.getOrDefault(nums[r], 0) + 1);
+
+            while (Collections.max(freq.keySet()) - Collections.min(freq.keySet()) > 2) {
+                freq.put(nums[l], freq.get(nums[l]) - 1);
+                if (freq.get(nums[l]) == 0) {
+                    freq.remove(nums[l]);
+                }
+                l++;
+            }
+
+            ans += r - l + 1;
+            r++;
+        }
+
+        return ans;
+    }
+}
